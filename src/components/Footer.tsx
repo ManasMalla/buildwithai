10e3bdb50a6772744e08c814e9ad4245c9141ad7
Manas Malla <manasmalla.dev@gmail.com>
2024-05-17 22:41:05 +0530

    added gdg logo
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="bg-[#fafafa] dark:bg-[#202023]/30  md:mt-16 px-[20px] md:px-16 md:pt-16 pt-7 pb-24 md:pb-9 md:h-52 flex flex-col justify-between font-medium relative z-10 lg:-translate-y-[48px]">
-      <img src="inline_logo.svg" className="h-16 w-auto mr-auto" />
+      <img src="/images/gdg-logo.png" className="h-10 w-auto mr-auto" />
       <div className="flex flex-col md:flex-row justify-start md:items-center space-y-[14px] md:space-y-[unset] md:space-x-9 text-[13px] md:text-[14px] text-[#727272] cursor-pointer">
         <a href="/#" className=" hover:text-[#1a73e8] font-medium">
           Build With AI
